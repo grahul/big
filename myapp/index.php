8811fff770e52783684cd7823eb34b5be04c3f9c
RahulG <rahulg.active@gmail.com>
2015-04-02 18:02:31 +0530

    type your commit message here

--- myapp/index.php
+++ myapp/index.php
@@ -9,5 +9,9 @@
 	echo "hi";
 	?>
 <h1>RAHULG GUPTA</h1>
+<center>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sapiente nulla corporis aliquid sequi debitis pariatur quibusdam libero impedit dolorum? Quod, sunt omnis atque accusamus aperiam ipsa distinctio minima a?</center>
 </body>
 </html>
+git remote add origin https://github.com/username/rails-girls.git
+git remote add origin https://github.com/grahul/big.git
+
