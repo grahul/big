27769216048ea1b29c8342d1b027909a67e1decb
RahulG <rahulg.active@gmail.com>
2015-04-04 00:51:48 +0530

    Now

--- myapp/index.php
+++ myapp/index.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
 </head>
 <body>
-	<?php
-	echo "hi";
-	?>
-
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum incidunt repellat, rerum quidem, quo sunt velit aperiam maiores laudantium excepturi blanditiis! Repellat ut soluta sapiente sit illo odio quas eos.
-
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quas excepturi, quia vel, mollitia voluptatem non nostrum itaque autem nemo at quos. Ut perferendis, veniam numquam harum laborum aperiam dolorem.
-
-sd
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti tempora ad officia ab molestias ullam esse fugiat sint beatae aperiam minus culpa suscipit labore provident, nulla natus quas at facere.
-	
-	asdassadsdasLorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nulla accusantium placeat odio, sint repellendus quod, in possimus. Consequatur, possimus delectus quae quasi debitis, eligendi praesentium omnis ab magnam hic.
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint praesentium quis possimus laudantium natus soluta deleniti fugit tempora ipsum minima, est, voluptate repudiandae, amet eos quas illum autem ipsam optio.
-<h1>RAHULG GUPTA</h1>
-<center>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sapiente nulla corporis aliquid sequi debitis pariatur quibusdam libero impedit dolorum? Quod, sunt omnis atque accusamus aperiam ipsa distinctio minima a?</center>
+	<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+	<a class="navbar-brand" href="#">Title</a>
+	<ul class="nav navbar-nav">
+		<li class="active">
+			<a href="#">Home</a>
+		</li>
+		<li>
+			<a href="#">Link</a>
+		</li>
+	</ul>
+</nav>
 </body>
 </html>
-
