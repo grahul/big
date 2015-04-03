67caaeaaf33e92d56b8f0bde62ca5ba2e7973764
RahulG <rahulg.active@gmail.com>
2015-04-03 23:36:48 +0530

    opps

--- myapp/index.php
+++ myapp/index.php
@@ -5,14 +5,10 @@
 	<title>Document</title>
 </head>
 <body>
-Lorem ipsum dolor sit amet, consectetur adi
-asdasdad
-asdasd
-asd
-asdasdaspisicing elit. Impedit, minus beatae quae deleniti, tempora in accusantium, cumque itaque, ab id repudiandae aspernatur architecto asperiores neque. Quas quaerat ipsum quod aliquam.
 	<?php
 	echo "hi";
 	?>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nulla accusantium placeat odio, sint repellendus quod, in possimus. Consequatur, possimus delectus quae quasi debitis, eligendi praesentium omnis ab magnam hic.
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint praesentium quis possimus laudantium natus soluta deleniti fugit tempora ipsum minima, est, voluptate repudiandae, amet eos quas illum autem ipsam optio.
 <h1>RAHULG GUPTA</h1>
 <center>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sapiente nulla corporis aliquid sequi debitis pariatur quibusdam libero impedit dolorum? Quod, sunt omnis atque accusamus aperiam ipsa distinctio minima a?</center>
